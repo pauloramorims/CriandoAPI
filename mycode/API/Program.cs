1e1040d1ca978c92446807b4c2b4104eac8c5a74
Paulo Ricardo Amorim <pauloramorims@gmail.com>
2022-01-16 14:06:27 -0300

    Avancando em minha primeira API

--- mycode/API/Program.cs
+++ mycode/API/Program.cs
@@ -1,6 +1,40 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Mvc;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);   /*resposonsavel por criar minha aplicacao web   */
+var app = builder.Build();                          /*nosso hosting, aquele que vai escutar oq o usuario quer acessar*/
+
+//endpoint abaixo!
+app.MapGet("/", () => "Hello World! 5");  //toda vez que digitar o endereco da minha aplicacao e barra
+
+app.MapPost ("/", () => new {Name = "Paulo", Age= 25 }); //Criando Objeto Anônimo, o proprio C# converte para o tipo JSON
+
+//Nesta expressão lambda tudo após o "=>" é o corpo do meu método
+app.MapGet ("/AddHeader", (HttpResponse response) => //Estou adicionando um cabeçalho e retornando um objeto.
+{
+    response.Headers.Add("Teste","Paulo Ricardo");
+    return new {Name = "Paulo", Age= 25 };
+});
+
+app.MapPost("/sabeproduct", (Product product) => {  //Enviar uma informação atraves do Body//
+    return product.Code + " - " + product.Name;
+});
+
+//↓↓ Extrair relatório do meu endpoint: Numero de usuários que acessou meu sistema
+//↓↓ Através de parâmetros via URL: 2 tipos
+
+//api.app.com/users?datestart={date}&dateend={date}  → Via carry string
+app.MapGet("/getproduct", ([FromQuery]string dateStart, [FromQuery] string dateEnd) => {
+    return dateStart + " - " + dateEnd;
+});
+
+//api.app.com/users/{code}  → Via rota
+app.MapGet("/getproduct/{code}", ([FromRoute]string code) => {
+    return code;
+});
 
 app.Run();
+
+public class Product {
+    public string Code { get; set; }
+    public string Name { get; set; }
+}
