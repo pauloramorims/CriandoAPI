a7acedaf1b1ff622131bf58ff2ceae1cefd0e9d2
Paulo Ricardo <pauloramorims@gmail.com>
2022-01-17 18:07:29 -0300

    Adicioanndo endpoints

--- mycode/API/Program.cs
+++ mycode/API/Program.cs
@@ -17,32 +17,63 @@ app.MapGet ("/AddHeader", (HttpResponse response) => //Estou adicionando um cabe
     return new {Name = "Paulo", Age= 25 };
 });
 
-app.MapPost("/sabeproduct", (Product product) => {  //Enviar uma informação atraves do Body//
-    return product.Code + " - " + product.Name;
+app.MapPost("/products", (Product product) => {  //Enviar uma informação atraves do Body//Adicionando um produto a minha lista
+    ProductRepository.Add(product);
 });
 
 //↓↓ Extrair relatório do meu endpoint: Numero de usuários que acessou meu sistema
 //↓↓ Através de parâmetros via URL: 2 tipos
 
-//api.app.com/users?datestart={date}&dateend={date}  → Via carry string
+/*/api.app.com/users?datestart={date}&dateend={date}  → Via carry string
 app.MapGet("/getproduct", ([FromQuery]string dateStart, [FromQuery] string dateEnd) => {
     return dateStart + " - " + dateEnd;
-});
+});*/
 
 //api.app.com/users/{code}  → Via rota
-app.MapGet("/getproduct/{code}", ([FromRoute]string code) => {
-    return code;
+app.MapGet("/products/{code}", ([FromRoute]string code) => {
+    var product = ProductRepository.GetBy(code);
+    return product;
 });
 
 //→ Via Header
 app.MapGet("/getproductbyheader", (HttpRequest request) => 
 {
-    request.Header["product-code"].ToString();
+    request.Headers["product-code"].ToString();
 }); //HttpRequest é responsável por receber a solicitação do usuário no endpoint
+
+app.MapPut("/products", (Product product) => {    //endpoint para editar meus produtos
+    var productSaved = ProductRepository.GetBy(product.Code);
+    productSaved.Name = product.Name;
+});
+
+app.MapDelete("/products/{code}", ([FromRoute]string code) => {
+    var productSaved = ProductRepository.GetBy(code);
+    ProductRepository.Remove(productSaved);
+    
+});
 
 app.Run();
 
-public class Product {
+//Toda vez que rodo meu servidor a memoria será perdida para um método do tipo List, se estivesse em um banco de dados isso nãoa conteceria pois estaria num arquivo e não na memória...
+public static class ProductRepository{ //A cada requisição que eu realizar, a classe é construida do zero. Para q isso n aconteça utilizamos 'static'
+    public static List<Product> Products { get; set; } //Método para adicionar um poduto
+    public static void Add(Product product) //Método para adicionar um poduto
+    {
+        if (Products == null)   //Conferindo se alista está vazia
+            Products = new List<Product>();
+
+        Products.Add(product);
+    }
+
+    public static Product GetBy(string code){ //Método para procurar um poduto
+        return Products.FirstOrDefault(p => p.Code == code); //Se não achar na lista, retorna um valor "null"
+    }
+
+    public static void Remove(Product product){ //Método para remover um poduto
+        Products.Remove(product);
+    }
+}
+public class Product { //Minha tabela
     public string Code { get; set; }
     public string Name { get; set; }
 };
